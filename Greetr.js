52a2f52f36cdfe2a18a0c9a6d57f0255641693d7
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2022-12-06 10:46:50 +0530

    initialize function constructor

--- Greetr.js
+++ Greetr.js
@@ -1,1 +1,12 @@
-(function (global, $) {})(window, jQuery);
+(function (global, $) {
+  var Greetr = function (firstName, lastName, language) {
+    return new Greetr.init(firstName, lastName, language);
+  };
+
+  Greetr.init = function (firstName, lastName, language) {
+    var self = this;
+    self.firstName = firstName || "";
+    self.lastName = lastName || "";
+    self.language = language || "es";
+  };
+})(window, jQuery);
